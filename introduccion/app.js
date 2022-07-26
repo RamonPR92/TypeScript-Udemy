94efab1a27bf39a3a144f84c51208945690c2132
RamonPR92 <ramonpr92@hotmail.com>
2022-07-25 20:48:06 -0500

    Seccion de introduccion

---
+++ introduccion/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const mensaje = 'Hola mundo';
+console.log(mensaje);
+const hero = { name: 'ironman', edad: 34 };
+hero.edad = 50;
+console.log(hero.edad);
---
+++ introduccion/app.ts
@@ -0,0 +1,7 @@
+
+const mensaje:string = 'Hola mundo';
+console.log(mensaje);
+
+const hero = { name: 'ironman', edad: 34};
+hero.edad = 50;
+console.log(hero.edad)
